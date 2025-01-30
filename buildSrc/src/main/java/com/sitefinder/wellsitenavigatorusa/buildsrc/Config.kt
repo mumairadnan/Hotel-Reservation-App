c40fc1351bccc0bfb52a02660da5ddddf2b6a3c4
Umair Adnan Shafi <97875076+umairadnan@users.noreply.github.com>
2025-01-30 22:00:33 +0500

    Delete buildSrc/src/main/java/com/sitefinder/wellsitenavigatorusa/buildsrc directory

--- buildSrc/src/main/java/com/sitefinder/wellsitenavigatorusa/buildsrc/Config.kt
+++
@@ -1,9 +0,0 @@
-package com.sitefinder.wellsitenavigatorusa.buildsrc
-
-object Config {
-    const val COMPILE_SDK_VERSION = 34
-    const val MIN_SDK_VERSION = 26
-    const val TARGET_SDK_VERSION = COMPILE_SDK_VERSION
-    const val VERSION_CODE = 386
-    const val VERSION_NAME = "5.8.4"
-}
